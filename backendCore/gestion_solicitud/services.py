64342ddb68745646cf3a4c5fc76c67bb67f94a96
Berkutv3 <gabriel.cruz2@unmsm.edu.pe>
2024-07-18 18:25:13 -0500

    fixed circular import

--- backendCore/gestion_solicitud/services.py
+++ backendCore/gestion_solicitud/services.py
@@ -1,8 +1,8 @@
-from .models import TablaPrecios
+from django.utils.crypto import get_random_string
 
 ## Implement your extra business logic here
 
-def calculate_price(paquete):
+def calculate_price(paquete, TablaPrecios):
     try:
         price_entry = TablaPrecios.objects.get(peso=paquete.peso)
         return price_entry.precio
@@ -19,3 +19,9 @@ def external_payment_service(pedido):
     # Call external payment service
     # ¿If payment is successful return true?
     return True
+
+def generate_unique():
+    return get_random_string(length=32)
+
+def generate_unique8():
+    return get_random_string(length=8)
