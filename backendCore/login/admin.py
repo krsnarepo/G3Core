d291238b83ef161f76a72842577a5374eb1ab87a
Berkutv3 <gabriel.cruz2@unmsm.edu.pe>
2024-07-18 21:26:04 -0500

    Added missing model to admin.py

--- backendCore/login/admin.py
+++ backendCore/login/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Worker, WorkerType
+from .models import Worker, WorkerType, Cliente
 
 # Register your models here.
 
 admin.site.register(Worker)
 admin.site.register(WorkerType)
+admin.site.register(Cliente)
