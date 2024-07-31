8c8b75ca94d7c46c4ce0153ea21d1c27b5c02780
Berkutv3 <gabriel.cruz2@unmsm.edu.pe>
2024-07-31 01:28:34 -0500

    Fixed name mismatch permissions

--- backendCore/login/permissions.py
+++ backendCore/login/permissions.py
@@ -2,16 +2,16 @@ from rest_framework import permissions
 
 class IsManager(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user and request.user.is_authenticated and request.user.worker.worker_type.name == 'Jefe de Logistica'
+        return request.user and request.user.is_authenticated and request.user.worker.worker_type.name == 'manager'
 
 class IsReceptionist(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user and request.user.is_authenticated and request.user.worker.worker_type.name == 'Recepcionista'
+        return request.user and request.user.is_authenticated and request.user.worker.worker_type.name == 'recepcionist'
 
 class IsWarehouseWorker(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user and request.user.is_authenticated and request.user.worker.worker_type.name == 'Trabajador de Almacen'
+        return request.user and request.user.is_authenticated and request.user.worker.worker_type.name == 'warehouse'
 
 class IsDelivery(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user and request.user.is_authenticated and request.user.worker.worker_type.name == 'Repartidor'
+        return request.user and request.user.is_authenticated and request.user.worker.worker_type.name == 'delivery'
--- backendCore/login/signals.py
+++ backendCore/login/signals.py
@@ -1,11 +1,13 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
-from .models import Worker, WorkerType
+from .models import Worker, WorkerType, Cliente
 
 @receiver(post_save, sender=User)
 def create_worker(sender, instance, created, **kwargs):
     if created:
-        # Assuming you have a default WorkerType to assign to new workers
-        default_worker_type, created = WorkerType.objects.get_or_create(name='Cliente')
+        # Assuming there is a default WorkerType to assign to new workers
+        default_worker_type = WorkerType.objects.get(name='client')
+        # default_worker_type, created = WorkerType.objects.get_or_create(name='client')
         Worker.objects.create(user=instance, worker_type=default_worker_type)
+        Cliente.objects.create(user=instance)
